53dc7e4c465c6c8a1f6d12b572e8771a51a8367e
Jhon Adams <githu@l124rd.com>
2013-02-03 14:25:21 -0800

    Move to maven for build sytem

--- go.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -x
-CLASSPATH=../craftbukkit-1.2.5-R2.0.jar javac *.java -Xlint:deprecation -Xlint:unchecked
-rm -rf me
-mkdir -p me/exphc/RealisticChat
-mv *.class me/exphc/RealisticChat/
-jar cf RealisticChat.jar me/ *.yml *.java README.md ChangeLog LICENSE
